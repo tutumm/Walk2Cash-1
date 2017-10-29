69586747cc53527476303ac8bfae7c443ca76bcd
Yanisa Hemprachitchai <aiya.yanisa@gmail.com>
2017-10-29 12:26:22 +0700

    UserScreen Profile picture, name, buttons and graph

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -3,23 +3,69 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Button
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux'
+import Chart from 'react-native-chart';
+
+const data = [[
+	[0, 1],
+	[1, 3],
+	[3, 7],
+	[4, 9],
+]];
 
 
 class UserScreen extends Component {
 
     render() {
-
+        const buttons = ['Hello', 'World', 'Buttons']
         return (
             <View style = {styles.container}>
-                <Text 
-                    style = {styles.welcome}
-                >
-                    User Screen
-                </Text>
+                 <Image
+                 style={{width: 100, height: 100, borderRadius: 50}}
+                 source={{uri: 'https://www.tkgourmet.com/v/vspfiles/photos/3-salmonnorway-2.jpg'}}
+               /> 
+               <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold',margin:15}}>Salmon Norway</Text>
+               
+               <View style={{flexDirection:'row', flexWrap:'wrap'}}>
+               <Button style={styles.buttontab}
+               /* onPress={onPressLearnMore} */
+               title="Day"
+               accessibilityLabel="day"
+             />
+             <Button style={styles.buttontab}
+               /* onPress={onPressLearnMore} */
+               title="Month"
+               accessibilityLabel="month"
+             />
+             <Button style={styles.buttontab}
+               /* onPress={onPressLearnMore} */
+               title="Year"
+               accessibilityLabel="year"
+             />
+               </View>
+
+               <View>
+				<Chart style={{
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'white',
+	}}
+					style={styles.chart}
+					data={data}
+					verticalGridStep={5}
+					type="line"
+					showDataPoint={true}
+					color={['#e1cd00']}
+				 />
+			</View>
+          
+
             </View>
         );
 
@@ -28,6 +74,13 @@ class UserScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+    buttontab: {
+        color :"#F5318D"
+    },
+    chart: {
+		width: 200,
+		height: 200,
+	},
     container: {
       flex: 1,
       justifyContent: 'center',

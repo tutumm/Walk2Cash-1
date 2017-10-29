24568c699775a9f1040b7f227e39daf76c6b25db
Yanisa Hemprachitchai <aiya.yanisa@gmail.com>
2017-10-29 13:04:17 +0700

    Change library for displaying bar graph

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -9,24 +9,27 @@ import {
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux'
-import Chart from 'react-native-chart';
+import { VictoryBar } from "victory-native";
+
+
 
 const data = [[
-	[0, 1],
-	[1, 3],
-	[3, 7],
-	[4, 9],
+  [0, 1],
+  [1, 3],
+  [3, 7],
+  [4, 9],
 ]];
-
 
 class UserScreen extends Component {
 
+
+
     render() {
-        const buttons = ['Hello', 'World', 'Buttons']
+        
         return (
             <View style = {styles.container}>
                  <Image
-                 style={{width: 100, height: 100, borderRadius: 50}}
+                 style={{width: 100, height: 100, borderRadius: 50,marginTop: 30}}
                  source={{uri: 'https://www.tkgourmet.com/v/vspfiles/photos/3-salmonnorway-2.jpg'}}
                /> 
                <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold',margin:15}}>Salmon Norway</Text>
@@ -49,22 +52,9 @@ class UserScreen extends Component {
              />
                </View>
 
-               <View>
-				<Chart style={{
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: 'white',
-	}}
-					style={styles.chart}
-					data={data}
-					verticalGridStep={5}
-					type="line"
-					showDataPoint={true}
-					color={['#e1cd00']}
-				 />
-			</View>
-          
+             
+            <VictoryBar />
+    
 
             </View>
         );
@@ -86,12 +76,6 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
       backgroundColor: '#171C2F',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-      color : '#F5318D'
     }
   });
   

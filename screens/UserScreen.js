be912574e70a1e4ff58a975cf8f1cd3cd6aeaf1e
Yanisa Hemprachitchai <aiya.yanisa@gmail.com>
2017-10-31 03:55:22 +0700

    DONE Bar graph and Total steps

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -10,8 +10,8 @@ import {
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux'
-import { BarChart } from 'react-native-charts'
 import Pie from 'react-native-pie'
+import Chart from 'react-native-chart';
 
 onPressLearnMore = () => {
 
@@ -22,23 +22,36 @@ class UserScreen extends Component {
   state = {
     view : -1,
     pressStatusDay: 1, 
+    pressStatusWeek: 0, 
     pressStatusMonth: 0, 
-    pressStatusYear: 0, 
+    pressStatusYear: 0    
   }
 
   onPressDay = () => {
     this.setState({
       view : 0,
       pressStatusDay : 1,
+      pressStatusWeek: 0, 
+      pressStatusMonth : 0,
+      pressStatusYear : 0,
+    
+    })
+  }
+  onPressWeek = () => {
+    this.setState({
+      view : 1,
+      pressStatusDay : 0,
+      pressStatusWeek: 1, 
       pressStatusMonth : 0,
       pressStatusYear : 0
-    
+      
     })
   }
   onPressMonth = () => {
     this.setState({
-      view : 1,
+      view : 2,
       pressStatusDay : 0,
+      pressStatusWeek: 0, 
       pressStatusMonth : 1,
       pressStatusYear : 0
       
@@ -46,193 +59,207 @@ class UserScreen extends Component {
   }
   onPressYear = () => {
     this.setState({
-      view : 2,
+      view : 3,
       pressStatusDay : 0,
+      pressStatusWeek: 0, 
       pressStatusMonth : 0,
       pressStatusYear : 1
       
     })
   }
 
-
   showGraph = () =>{
     if(this.state.view==1){
-      return (
-        <BarChart
-        dataSets={[
-        { 
-          fillColor: '#F5318D', 
-          data: [
-            { value: 15 },
-            { value: 10 },
-            { value: 12 },
-            { value: 10 },
-            { value: 12 },
-            { value: 11 },
-            { value: 11 },
-            { value: 15 },
-            { value: 10 },
-            { value: 12 },
-            { value: 11 },
-            { value: 11 },
-            { value: 15 },
-            { value: 10 },
-            { value: 12 },
-
-          ]
-        }
-      ]}
-        graduation={1}
-        horizontal={false}
-        showGrid={false}
-        barSpacing={5}
-        style={{
-          height: 200,
-          margin: 15,
-          width: 300
-      }}/>
+      return(
+        <Chart
+        style={styles.chart}
+        data={[
+            ["SAT", 4714],
+            ["SUN", 172],
+            ["MON", 6392],
+            ["TUE", 4707],
+            ["WED", 9023],
+            ["THU", 4714],
+            ["FRI", 1722],
+        ]}
+        verticalGridStep={2}
+        type="bar"
+        axisColor="#F72582"
+        axisLabelColor="#F72582"
+        hideVerticalGridLines={true}
+     />
       )
     }else if(this.state.view==2){
-      return (
-        <BarChart
-        dataSets={[
-        { 
-          fillColor: '#F5318D', 
-          data: [
-            { value: 15 },
-            { value: 10 },
-            { value: 12 },
-            { value: 11 },
-            { value: 15 },
-            { value: 10 },
-            { value: 12 },
-            { value: 11 },
-            { value: 15 },
-            { value: 12 },
-            { value: 11 },
-            { value: 15 },
-
-          ]
-        }
-      ]}
-        graduation={1}
-        horizontal={false}
-        showGrid={false}
-        barSpacing={5}
-        style={{
-          height: 200,
-          margin: 15,
-          width: 300
-        }}/>
+      return(
+        <Chart
+        style={styles.chart}
+        data={[
+            ["20/9", 4496],
+            ["", 2345],
+            ["", 6455],
+            ["", 6709],
+            ["", 5475],
+            ["25/9", 6392],
+            ["", 4707],
+            ["", 1364],
+            ["", 8023],
+            ["", 4714],
+            ["29/9", 172],
+            ["", 6392],
+            ["", 4707],
+            ["", 9023],
+            ["", 4714],
+            ["3/10", 1722],
+        ]}
+        verticalGridStep={2}
+        type="bar"
+        axisColor="#F72582"
+        axisLabelColor="#F72582"
+        hideVerticalGridLines={true}
+     />
+      )
+    }else if(this.state.view==3){
+      return(
+        <Chart
+        style={styles.chart}
+        data={[
+            ["JAN", 4496],
+            ["FEB", 5502],
+            ["MAR", 6455],
+            ["APR", 6709],
+            ["MAY", 5475],
+            ["JUN", 6392],
+            ["JUL", 4707],
+            ["AUG", 4333],
+            ["SEP", 4714],
+            ["OCT", 4524],
+        ]}
+        verticalGridStep={2}
+        type="bar"
+        axisColor="#F72582"
+        axisLabelColor="#F72582"
+        hideVerticalGridLines={true}
+     />
       )
     }else{
-      return (
-       <BarChart
-       dataSets={[
-       { 
-         fillColor: '#F5318D', 
-         data: [
-           { value: 15 },
-           { value: 10 },
-           { value: 12 },
-           { value: 11 },
-           { value: 15 },
-           { value: 10 },
-           { value: 12 }
-         ]
-       }
-     ]}
-       graduation={1}
-       horizontal={false}
-       showGrid={false}
-       barSpacing={5}
-       style={{
-         height: 200,
-         margin: 15,
-         width: 300
-       }}/>
+      return(
+        <Chart
+        style={styles.chart}
+        data={[
+            ["09:00", 34],
+            ["10:00", 60],
+            ["11:00", 12],
+            ["12:00", 44],
+            ["13:00", 345],
+            ["14:00", 120],
+            ["15:00", 125],
+            ["16:00", 92],
+            ["17:00", 61],
+            ["18:00", 72],
+        ]}
+        verticalGridStep={2}
+        type="bar"
+        axisColor="#F72582"
+        axisLabelColor="#F72582"
+        hideVerticalGridLines={true}
+     />
       )
-     }
+    }
   }
 
-  showGraphLabel = () => {
+  showTotal = () =>{
     if(this.state.view==1){
-      return (
+      return(
         <View style={{flexDirection:'row'}}>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 1</Text>
-          <Text style={styles.dayLabel}> 11</Text>
-          <Text style={styles.dayLabel}> 11</Text>
-          <Text style={styles.dayLabel}> 11</Text>
-          <Text style={styles.dayLabel}> 11</Text>
-          <Text style={styles.dayLabel}> 11</Text>
-          <Text style={styles.dayLabel}> 11</Text>
+          <View style={{alignItems: 'center',margin:10}}>
+            <Text style={{fontSize:15,color:'white'}}>Total steps</Text>
+            <Text style={{fontSize:20,color:'#F72582',fontWeight:'bold'}}>25,321 steps</Text>
+          </View>
+          <View style={{alignItems: 'center',margin:10}}>
+            <Text style={{fontSize:15,color:'white'}}>Avg steps</Text>
+            <Text style={{fontSize:20,color:'#F72582',fontWeight:'bold'}}>4,220 steps</Text>
+          </View>
         </View>
       )
-    }
-    else if(this.state.view==2){
-      return (
-        <View>
-          <Text style={{
-            color: 'white'
-          }}>
-            Hello
-          </Text>
+    }else if(this.state.view==2){
+      return(
+        <View style={{flexDirection:'row'}}>
+          <View style={{alignItems: 'center',margin:10}}>
+            <Text style={{fontSize:15,color:'white'}}>Total steps</Text>
+            <Text style={{fontSize:20,color:'#F72582',fontWeight:'bold'}}>135,505 steps</Text>
+          </View>
+          <View style={{alignItems: 'center',margin:10}}>
+            <Text style={{fontSize:15,color:'white'}}>Avg steps</Text>
+            <Text style={{fontSize:20,color:'#F72582',fontWeight:'bold'}}>4,672 steps</Text>
+          </View>
         </View>
       )
-    }
-    else{
-      return (
-        <View>
-          <Text>
-            eiei
-          </Text>
+    }else if(this.state.view==3){
+      return(
+        <View style={{flexDirection:'row'}}>
+          <View style={{alignItems: 'center',margin:10}}>
+            <Text style={{fontSize:15,color:'white'}}>Total steps</Text>
+            <Text style={{fontSize:20,color:'#F72582',fontWeight:'bold'}}>1,592,041 steps</Text>
+          </View>
+          <View style={{alignItems: 'center',margin:10}}>
+            <Text style={{fontSize:15,color:'white'}}>Avg steps</Text>
+            <Text style={{fontSize:20,color:'#F72582',fontWeight:'bold'}}>5,006 steps</Text>
+          </View>
+        </View>
+      )
+    }else{
+      return(
+        <View style={{alignItems: 'center',margin:10}}>
+          <Text style={{fontSize:15,color:'white'}}>Total steps</Text>
+          <Text style={{fontSize:20,color:'#F72582',fontWeight:'bold'}}>1,440 steps</Text>
         </View>
       )
     }
   }
-
-
     render() {
         
         return (
             <ScrollView contentContainerStyle = {styles.container}>
-                 <Image
-                 style={{width: 100, height: 100, borderRadius: 50,marginTop: 30}}
-                 source={{uri: 'https://www.tkgourmet.com/v/vspfiles/photos/3-salmonnorway-2.jpg'}}
-               /> 
-               <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold',margin:15}}>Salmon Norway</Text>
 
-               <Text style={styles.textstyle}>MY POINTS: {this.props.totalPoint} </Text>
-
+              <View style={{flexDirection:'row',margin:30}}>
+                  <View>
+                    <Image
+                        style={{width: 100, height: 100, borderRadius: 50,marginRight:30}}
+                        source={{uri: 'https://www.tkgourmet.com/v/vspfiles/photos/3-salmonnorway-2.jpg'}}
+                    /> 
+                  </View>
+                  <View style={{justifyContent:'center'}}>
+                    <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>Salmon Norway</Text>
+                    <Text style={{color: '#F5318D'}}>MY POINTS: 144 </Text>
+                  </View>
+              </View>
+              
+              
                <View style={{flexDirection:'row', flexWrap:'wrap'}}>
 
                     <View style={styles.sortGraph}>
                             
                             <Text style={this.state.pressStatusDay ? styles.buttonPress : styles.welcome} onPress={this.onPressDay}>
                                 Day</Text>
+                            <Text style={this.state.pressStatusWeek ? styles.buttonPress : styles.welcome} onPress={this.onPressWeek}>
+                                Week</Text>
                             <Text style={this.state.pressStatusMonth ? styles.buttonPress : styles.welcome} onPress={this.onPressMonth}>
                                 Month</Text>
                             <Text style={this.state.pressStatusYear ? styles.buttonPress : styles.welcome} onPress={this.onPressYear}>
                                 Year</Text>
                     </View>
               </View>
-              
 
-             <View>
+
+             <View style={{flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
                 {this.showGraph()}
               </View>
 
-              
               <View>
-                {this.showGraphLabel()}
+                {this.showTotal()}
               </View>
 
               <View style={styles.card}>
@@ -362,11 +389,11 @@ const styles = StyleSheet.create({
     welcome: {
       borderRadius: 50,
       height: 30,
-      width: 80,
+      width: 70,
       borderRadius:10,
       borderWidth: 1,
       borderColor: '#F72582',
-      margin: 5,
+      margin: 3,
       color: '#F72582',
       textAlign: 'center',
       textAlignVertical: 'center'
@@ -375,15 +402,19 @@ const styles = StyleSheet.create({
     buttonPress: {
       borderRadius: 50,
       height: 30,
-      width: 80,
+      width: 70,
       borderRadius:10,
-      margin: 5,
+      margin: 3,
       backgroundColor: '#F72582',
       textAlign: 'center',
       textAlignVertical: 'center'
         
     },
-    
+    chart: {
+      width: 340,
+      height: 200,
+      margin: 20
+    },
     container: {
       alignItems: 'center',
       backgroundColor: '#171C2F',
@@ -391,11 +422,6 @@ const styles = StyleSheet.create({
 
     sortGraph: {
       flexDirection: 'row'
-    },
-    textstyle: {
-      textAlign: 'center',
-      margin: 15,
-      color: '#F5318D'
     },
     gauge: {
       position: 'absolute',
@@ -412,7 +438,6 @@ const styles = StyleSheet.create({
     gaugeGoal: {
       color: '#364069',
       fontWeight: 'bold',
-      marginBottom: 5
     },
     card: {
       flexDirection: 'column',
@@ -421,7 +446,7 @@ const styles = StyleSheet.create({
       shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.6, shadowRadius: 10,
       width: 300,
       height: 370,
-      marginTop: 20,
+      margin: 20,
     },
     textDet: {
       color: 'white',

7257700840f0d83aa69ee1544672646c9f4977f1
Sakan Komolvatin <tutumm96@gmail.com>
2017-10-31 16:46:05 +0700

    Product detail acept stars ratings

--- screens/ProductDetail.js
+++ screens/ProductDetail.js
@@ -8,20 +8,80 @@ import {
 } from 'react-native';
 
 import { Actions } from 'react-native-router-flux'
+import { Col, Row, Grid } from "react-native-easy-grid"
 
+const imageName = {
+  image05: require('../images/stars/0.5.png'),
+  image1: require('../images/stars/1.png'),
+  image15: '../images/stars/1.5.png',
+  image2: '../images/stars/2.png',
+  image25: '../images/stars/2.5.png',
+  image3: '../images/stars/3.png',
+  image35: '../images/stars/3.5.png',
+  image4: '../images/stars/4.png',
+  image45: require('../images/stars/4.5.png'),
+  image5: '../images/stars/5.png'
+}
 
 class ProductDetail extends Component {
   productDetail = this.props.productDetail
+
+  componentDidMount() {
+
+    console.log(imageName['image' + '45'])
+  }
   render() {
     return (
       <View style={styles.container}>
-          <Text style={styles.productName}>{this.productDetail.name}</Text>
-          <Image style={{ width: 150, height: 150 }} source={{ uri: this.productDetail.img }} />
-          <Text>{this.productDetail.point}</Text>
+        <View style={{ height: 230, marginTop: 30, alignItems: 'flex-start' }}>
+          <Grid>
+            <Col style={{ alignItems: 'center' }}>
+              <Image style={{ width: 150, height: 150 }} source={{ uri: this.productDetail.img }} />
+            </Col>
+            <Col>
+              <Text style={styles.productBrand}>
+                {this.productDetail.brand}
+              </Text>
+              <Text style={styles.productName}>
+                {this.productDetail.name}
+              </Text>
+              <Text style={styles.productCategory}>
+                {this.productDetail.category}
+              </Text>
+              <Text style={styles.productRating}>
+                Ratings({this.productDetail.rating})
+              </Text>
+              <Image
+                  style={{ width: 124, height: 17, marginLeft: -10, marginTop: 10 }}
+                  source={this.getImage}
+                />
+            </Col>
+          </Grid>
+          <Grid style={{marginTop: 100}}>
+            <Col>
+              <Text style={{ marginTop: 13, alignItems: 'center', textAlign: 'center', justifyContent: 'center' }}>
+                <Text style={styles.productPoint}>{this.productDetail.point}</Text>
+                <Image
+                  style={{ width: 17, height: 17, marginLeft: 5 }}
+                  source={require('../images/Point.png')}
+                />
+              </Text>
+            </Col>
+            <Col>
+              <View style={styles.signUpButton}>
+                <Text style={{ textAlign: 'center', backgroundColor: 'transparent', fontSize: 18, color: 'white', marginTop: 3 }}>Claim</Text>
+              </View>
+            </Col>
+          </Grid>
+        </View>
+
+        <View style={styles.detail}>
+          <Text style={styles.productDetailHead}>รายละเอียด</Text>
+          <Text style={styles.productDetail}>{this.productDetail.detail}</Text>
+        </View>
       </View>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -30,12 +90,62 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#171C2F',
   },
+  productImg: {
+    alignItems: 'center'
+  },
   productName: {
-    fontSize: 30,
+    fontSize: 17,
+    color: 'white',
     fontWeight: 'bold',
-    color: 'white'
-  }
-  
+    marginRight: 10
+  },
+  productBrand: {
+    fontSize: 17,
+    color: 'white',
+  },
+  productCategory: {
+    paddingTop: 10,
+    fontSize: 17,
+    color: '#F72582',
+    opacity: 0.8
+  },
+  productRating: {
+    fontSize: 17,
+    color: 'white',
+    marginTop: 10
+  },
+  productPoint: {
+    fontSize: 17,
+    color: '#F72582',
+    textAlign: 'center',
+    marginTop: 10,
+    fontWeight: 'bold',
+    backgroundColor: 'transparent'
+  },
+  detail: {
+    width: '75%',
+  },
+  productDetail: {
+    color: 'white',
+    fontSize: 15,
+    opacity: 0.9
+  },
+  productDetailHead: {
+    color: 'white',
+    fontWeight: 'bold',
+    marginBottom: 10,
+    fontSize: 15
+  },
+  signUpButton: {
+    height: 35,
+    width: '80%',
+    borderRadius: 20,
+    borderWidth: 2,
+    backgroundColor: '#F72582',
+    borderColor: '#F72582'
+  },
+
+
 });
 
 export default ProductDetail
--- screens/RedeemScreen.js
+++ screens/RedeemScreen.js
@@ -41,22 +41,22 @@ class RedeemScreen extends Component {
 
   componentDidMount() {
     fetch('https://fb69ac76.ngrok.io/products')
-    .then((response) => response.json())
-    .then((data) => {
-      this.setState({
-        items: data
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          items: data
+        })
       })
-    })
-    .catch((error) => {
-      console.error(error)
-    });
+      .catch((error) => {
+        console.error(error)
+      });
   }
 
   renderCarousel(image, index) {
     return (
-        <View key={index}>
-            <Image style={{ width: BannerWidth, height: BannerHeight }} source={{ uri: image }} />
-        </View>
+      <View key={index}>
+        <Image style={{ width: BannerWidth, height: BannerHeight }} source={{ uri: image }} />
+      </View>
     );
   }
 
@@ -64,29 +64,37 @@ class RedeemScreen extends Component {
     const productDetail = {
       name: item.productName,
       detail: item.productDetail,
+      brand: item.productBrand,
       img: item.imgUrl,
       point: item.point,
+      rating: item.rating,
       category: item.category
     }
     return (
-      <TouchableHighlight style={styles.slide} onPress={() => Actions.productDetail({productDetail: productDetail})}>
+      <TouchableHighlight style={styles.slide} onPress={() => Actions.productDetail({ productDetail: productDetail })}>
         <View>
           <Image style={{ width: 150, height: 150 }} source={{ uri: item.imgUrl }} />
-          <Text style={styles.point}>{item.point}</Text>
+          <Text style={{marginTop: 10, alignItems: 'center', textAlign: 'center'}}>
+            <Text style={styles.point}>{item.point}</Text>
+            <Image
+              style={{ width: 17, height: 17, marginLeft: 5 }}
+              source={require('../images/Point.png')}
+            />
+          </Text>
         </View>
       </TouchableHighlight>
     );
   }
 
-  renderCategory (category, index){
+  renderCategory(category, index) {
     return (
       <View key={index}>
-          <Text style={styles.categoryButton}>{category}</Text>
+        <Text style={styles.categoryButton}>{category}</Text>
       </View>
     );
   }
 
-  render(){
+  render() {
     return (
       <ScrollView contentContainerStyle={styles.container}>
         <StatusBar
@@ -96,9 +104,9 @@ class RedeemScreen extends Component {
 
         <View style={styles.searchBox}>
           <TextInput style={styles.searchText}
-           onChangeText={(searchVoucher) => this.setState({searchVoucher})}
-           placeholder=' Search Voucher ...' 
-           placeholderTextColor='rgba(255, 255, 255, 0.5)' />
+            onChangeText={(searchVoucher) => this.setState({ searchVoucher })}
+            placeholder=' Search Voucher ...'
+            placeholderTextColor='rgba(255, 255, 255, 0.5)' />
         </View>
 
         <View style={styles.carousel}>
@@ -108,28 +116,28 @@ class RedeemScreen extends Component {
             loop
             index={0}
             pageSize={BannerWidth}
-            style={{height:10}}
+            style={{ height: 10 }}
           >
             {images.map((image, index) => this.renderCarousel(image, index))}
           </Carousel>
         </View>
 
         <View style={styles.justForYouText}>
-          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>Just For You</Text>
+          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>Just For You</Text>
         </View>
 
-        <View style={{height: 190}}>
+        <View style={{ height: 190 }}>
           <CarouselJustForYou
-              ref={(c) => { this._carousel = c; }}
-              data={this.state.items}
-              renderItem={this._renderItem}
-              sliderWidth={BannerWidth}
-              itemWidth={150}
-            />
+            ref={(c) => { this._carousel = c; }}
+            data={this.state.items}
+            renderItem={this._renderItem}
+            sliderWidth={BannerWidth}
+            itemWidth={150}
+          />
         </View>
 
         <View style={styles.categories}>
-          <Text style={{color: 'white', fontSize: 16, fontWeight: 'bold'}}>Categories</Text>
+          <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>Categories</Text>
           {categories.map((category, index) => this.renderCategory(category, index))}
         </View>
       </ScrollView>
@@ -138,73 +146,70 @@ class RedeemScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      alignItems: 'center',
-      backgroundColor: '#171C2F',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-      color : '#F5318D'
-    },
-    carousel: {
-      height: 180
-    },
-    searchBox: {
-      backgroundColor: '#262E46',
-      height: 50,
-      width: 300,
-      marginTop: 20,
-      marginBottom: 20,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.6,
-      shadowRadius: 10
-    },
-    searchText: {
-      fontSize: 14,
-      marginTop: 16,
-      marginLeft: 15,
-      color: 'white'
-    },
-    justForYouText: {
-      width: 250,
-      marginTop: 30,
-      marginBottom: 10
-    },
-    slide: {
-      height: 150,
-      width: 150,
-      backgroundColor: 'transparent'
-    },
-    point: {
-      color: '#F72582',
-      textAlign: 'center',
-      marginTop: 10,
-      fontWeight: 'bold',
-      shadowColor: '#F72582',
-      shadowOffset: { width: 0, height: 0 },
-      shadowOpacity: 0.9,
-      shadowRadius: 3
-    },
-    categories: {
-      width: 250,
-      marginBottom: 100
-    },
-    categoryButton: {
-      fontSize: 20,
-      marginTop: 10,
-      paddingTop: 3,
-      borderRadius: 50,
-      height: 35,
-      width: 250,
-      borderRadius:10,
-      borderWidth: 1,
-      borderColor: '#F72582',
-      color: '#F72582',
-      textAlign: 'center',
-    }
-  });
-  
-  export default RedeemScreen
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#171C2F',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#F5318D'
+  },
+  carousel: {
+    height: 180
+  },
+  searchBox: {
+    backgroundColor: '#262E46',
+    height: 50,
+    width: 300,
+    marginTop: 20,
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.6,
+    shadowRadius: 10
+  },
+  searchText: {
+    fontSize: 14,
+    marginTop: 16,
+    marginLeft: 15,
+    color: 'white'
+  },
+  justForYouText: {
+    width: 250,
+    marginTop: 30,
+    marginBottom: 10
+  },
+  slide: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'transparent'
+  },
+  point: {
+    fontSize: 17,
+    color: '#F72582',
+    textAlign: 'center',
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  categories: {
+    width: 250,
+    marginBottom: 100
+  },
+  categoryButton: {
+    fontSize: 20,
+    marginTop: 10,
+    paddingTop: 3,
+    borderRadius: 50,
+    height: 35,
+    width: 250,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#F72582',
+    color: '#F72582',
+    textAlign: 'center',
+  }
+});
+
+export default RedeemScreen
